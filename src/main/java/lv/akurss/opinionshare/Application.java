c225793f9027146b39b3d57aa59bed3c12b6062f
Aleksandrs Kartisevs <aleksandrs.kartisevs@ideaportriga.lv>
2018-03-17 12:18:42 +0200

    security and mail

--- src/main/java/lv/akurss/opinionshare/Application.java
+++ src/main/java/lv/akurss/opinionshare/Application.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EntityScan(
 		basePackageClasses = {
@@ -11,6 +12,7 @@ import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 				Jsr310JpaConverters.class}
 )
 @SpringBootApplication
+@EnableScheduling
 public class Application {
 
 	public static void main(String[] args) {
---
+++ src/main/java/lv/akurss/opinionshare/service/SpamService.java
@@ -0,0 +1,32 @@
+package lv.akurss.opinionshare.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SpamService {
+
+	private static final Logger log = LoggerFactory.getLogger(SpamService.class);
+	
+	@Autowired
+	private JavaMailSender mailSender;
+	
+	@Scheduled(cron = "0 */1 10 * * MON-SAT")
+	public void sendSpam() {
+		log.info("Sending mails");
+		
+		SimpleMailMessage mailMessage = new SimpleMailMessage();
+		mailMessage.setTo("alexkartishev@gmail.com");
+		mailMessage.setSubject("Test message");
+		mailMessage.setText("Test test");
+		mailMessage.setFrom("test@test.com");
+		
+		mailSender.send(mailMessage);
+	}
+	
+}
